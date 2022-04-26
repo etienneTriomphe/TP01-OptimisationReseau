1bcbd3695ab1e04f349033fa0bb5b7a80389e56f
etienneTriomphe <etienne.triomphe@cpe.fr>
2022-04-26 17:48:35 +0200

    tp1 à finir les templates

--- scripts/__main__.py
+++ scripts/__main__.py
@@ -1,6 +1,6 @@
 import json
 from jinja2 import Template, Environment, FileSystemLoader
-
+import yaml
 
 def load_json_data_from_file(file_path):
         
@@ -21,10 +21,16 @@ def load_json_data_from_file(file_path):
 
 
 def load_yaml_data_from_file(file_path):
-    """
-        A compléter ....
-    """
-    pass
+    try:
+        with open(file_path) as stream:
+            try:
+                data = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print("YAML error: ", exc)
+    except IOError:
+        print("Could not read file:", file_path)
+
+    return data
 
 
 def render_network_config(template_name, data):
@@ -48,9 +54,15 @@ def save_built_config(file_name, data):
 
 if __name__ == "__main__":
     #print(load_json_data_from_file("./data/ESW2.json"))
+    
     env = Environment(loader=FileSystemLoader("templates"))
 
     #print(render_network_config("ESW2.j2",load_json_data_from_file("./data/ESW2.json")))
     #print(render_network_config("R2.j2",load_json_data_from_file("./data/R2.json")))
     save_built_config("./config/ESW2.conf",render_network_config("ESW2.j2",load_json_data_from_file("./data/ESW2.json")))
     save_built_config("./config/R2.conf",render_network_config("R2.j2",load_json_data_from_file("./data/R2.json")))
+    
+    #print(load_yaml_data_from_file("./data/ESW4.yaml"))
+    #print(load_yaml_data_from_file("./data/R2.yaml"))
+    save_built_config("./config/ESW4_from_yaml.conf",render_network_config("ESW2.j2",load_yaml_data_from_file("./data/ESW4.yaml")))
+    save_built_config("./config/R2_from_yaml.conf",render_network_config("R2.j2",load_yaml_data_from_file("./data/R2.yaml")))
