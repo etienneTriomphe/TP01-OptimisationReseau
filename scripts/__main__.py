21d68e3644f66cc9f5622a4c33a3899262cd787d
etienneTriomphe <etienne.triomphe@cpe.fr>
2022-04-26 11:39:15 +0200

    generation automatique de la configuration reseau

--- scripts/__main__.py
+++ scripts/__main__.py
@@ -1,11 +1,23 @@
 import json
+from jinja2 import Template, Environment, FileSystemLoader
 
 
 def load_json_data_from_file(file_path):
-    """
-        A compléter ....
-    """
-    pass
+        
+    #version qui lève une erreur si le fichier ou le chemin est inexistant
+    #f = open(file_path)
+    #data = json.load(f)
+    #f.close()
+    
+    try:
+        with open(file_path) as f:
+            data = json.load(f)
+            f.close()
+    
+    except IOError:
+        print("Could not read file:", file_path)
+
+    return data
 
 
 def load_yaml_data_from_file(file_path):
@@ -16,18 +28,29 @@ def load_yaml_data_from_file(file_path):
 
 
 def render_network_config(template_name, data):
-    """
-        A compléter ....
-    """
-    pass
+    
+    template = env.get_template(template_name) 
+    return template.render(data) 
+
 
 
 def save_built_config(file_name, data):
-    """
-        A compléter ....
-    """
-    pass
+    
+    try:
+        with open(file_name, "w") as f:
+            f.write(data)
+            f.close()
+    
+    except IOError:
+        print("Could not read file:", file_name)
+
 
 
 if __name__ == "__main__":
-    pass
+    #print(load_json_data_from_file("./data/ESW2.json"))
+    env = Environment(loader=FileSystemLoader("templates"))
+
+    #print(render_network_config("ESW2.j2",load_json_data_from_file("./data/ESW2.json")))
+    #print(render_network_config("R2.j2",load_json_data_from_file("./data/R2.json")))
+    save_built_config("./config/ESW2.conf",render_network_config("ESW2.j2",load_json_data_from_file("./data/ESW2.json")))
+    save_built_config("./config/R2.conf",render_network_config("R2.j2",load_json_data_from_file("./data/R2.json")))
