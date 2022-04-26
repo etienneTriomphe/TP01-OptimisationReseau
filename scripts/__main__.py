ef6dbee7f7f04b74d1457dea5adc12600aaafa3d
etienneTriomphe <etienne.triomphe@cpe.fr>
2022-04-26 09:51:36 +0200

    premier commit

---
+++ scripts/__main__.py
@@ -0,0 +1,33 @@
+import json
+
+
+def load_json_data_from_file(file_path):
+    """
+        A compléter ....
+    """
+    pass
+
+
+def load_yaml_data_from_file(file_path):
+    """
+        A compléter ....
+    """
+    pass
+
+
+def render_network_config(template_name, data):
+    """
+        A compléter ....
+    """
+    pass
+
+
+def save_built_config(file_name, data):
+    """
+        A compléter ....
+    """
+    pass
+
+
+if __name__ == "__main__":
+    pass
